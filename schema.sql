cfa2ae015f10ed1e0c6e4dd31e0a684e773e7787
Kaylee Horner <kayleeahorner@gmail.com>
2020-12-05 00:23:45 -0600

    added base schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS tracker;
+
+CREATE DATABASE tracker;
+
+USE tracker;
+
+CREATE TABLE department(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL 
+, name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
+, title VARCHAR(30) NOT NULL
+, salary DECIMAL NOT NULL
+, department_id INT NOT NULL
+);
+
+CREATE TABLE employee(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
+, first_name VARCHAR(30) NOT NULL
+, last_name VARCHAR(30) NOT NULL
+, role_id INT NOT NULL
+, manager_id INT NULL
+);
+
+
+INSERT INTO department (name)
+VALUES ("Merchandising");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Associate Merchandiser", 37000 , 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kaylee", "Horner", 1, 1);
