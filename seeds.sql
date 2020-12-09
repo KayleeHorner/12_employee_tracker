a3810e4477620fc721587dbc35f8fa9c70b014fb
Kaylee Horner <kayleeahorner@gmail.com>
2020-12-08 22:42:27 -0600

    removed

--- seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO department (name)
-VALUES ("Merchandising");
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Associate Merchandiser", 37000 , 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kaylee", "Horner", 1, 1);
